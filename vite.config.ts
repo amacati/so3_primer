0406e7a803523b674b439c6c9270ee3666307f3d
Martin Schuck <martin.schuck@tum.de>
2026-02-05 00:44:23 +0100

    Fix website

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,23 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-    base: '/so3_primer_website/',
+    base: '/so3_primer/',
     build: {
         outDir: 'dist',
         assetsDir: 'assets',
         sourcemap: false,
+        chunkSizeWarningLimit: 600,
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes('node_modules/three')) {
+                        return 'three';
+                    }
+                    if (id.includes('node_modules/d3')) {
+                        return 'd3';
+                    }
+                }
+            }
+        }
     },
 })
